b11c58e492942f298dc431793b15528908efd1b1
Han Damin <miniex@daminstudio.net>
2024-10-24 20:57:15 +0900

    delete: rose-pine theme

--- lua/settings/themes.lua
+++ lua/settings/themes.lua
@@ -1,18 +1,5 @@
 return {
     {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        lazy = false,
-        config = function()
-            require("rose-pine").setup({
-                variant = "moon",
-                dark_variant = "moon",
-            })
-
-            vim.cmd([[colorscheme rose-pine]])
-        end,
-    },
-    {
         "scottmckendry/cyberdream.nvim",
         name = "cyberdream",
         lazy = false,
