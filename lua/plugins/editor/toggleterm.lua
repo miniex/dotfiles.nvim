87e7ee3e35c380aa610b822f00c8feaa6f9c8e68
Han Damin <miniex@daminstudio.net>
2025-09-02 03:03:59 +0900

    update: 2025-09-02

--- lua/plugins/editor/toggleterm.lua
+++ lua/plugins/editor/toggleterm.lua
@@ -1,17 +1,23 @@
 local map_key = require("utils.key_mapper").map_key
 
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    config = function()
-      require("toggleterm").setup({
-        direction = "float",
-        float_opts = {
-          border = "double",
-        },
-      })
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                direction = "float",
+                float_opts = {
+                    border = "double",
+                },
+                on_open = function(term)
+                    local opts = { buffer = term.bufnr }
+                    vim.keymap.set("n", "<Esc>", ":ToggleTerm<CR>", opts)
+                    vim.keymap.set("n", "q", ":ToggleTerm<CR>", opts)
+                    vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:ToggleTerm<CR>", opts)
+                end,
+            })
 
-      map_key("<leader>t", ":ToggleTerm<CR>")
-    end,
-  },
+            map_key("<leader>t", ":ToggleTerm<CR>")
+        end,
+    },
 }
