549d2fb3cb3efe7da0b85650f8638457d1a6eff1
Han Damin <miniex@daminstudio.net>
2025-10-27 08:25:01 +0900

    update: dashboard

--- lua/plugins/lang/tailwind.lua
+++ lua/plugins/lang/tailwind.lua
@@ -23,21 +23,69 @@ return {
             },
             setup = {
                 tailwindcss = function(_, opts)
-                    local tw = require("lspconfig.server_configurations.tailwindcss")
+                    -- Get default filetypes from lspconfig
+                    local default_filetypes = {
+                        "aspnetcorerazor",
+                        "astro",
+                        "astro-markdown",
+                        "blade",
+                        "clojure",
+                        "django-html",
+                        "htmldjango",
+                        "edge",
+                        "eelixir",
+                        "elixir",
+                        "ejs",
+                        "erb",
+                        "eruby",
+                        "gohtml",
+                        "gohtmltmpl",
+                        "haml",
+                        "handlebars",
+                        "hbs",
+                        "html",
+                        "html-eex",
+                        "heex",
+                        "jade",
+                        "leaf",
+                        "liquid",
+                        "mdx",
+                        "mustache",
+                        "njk",
+                        "nunjucks",
+                        "php",
+                        "razor",
+                        "slim",
+                        "twig",
+                        "css",
+                        "less",
+                        "postcss",
+                        "sass",
+                        "scss",
+                        "stylus",
+                        "sugarss",
+                        "javascript",
+                        "javascriptreact",
+                        "reason",
+                        "rescript",
+                        "typescript",
+                        "typescriptreact",
+                        "vue",
+                        "svelte",
+                    }
 
-                    if not tw or not tw.default_config then
-                        return
-                    end
+                    -- Start with default filetypes
+                    opts.filetypes = opts.filetypes or vim.deepcopy(default_filetypes)
 
-                    opts.filetypes = opts.filetypes or {}
-                    vim.list_extend(opts.filetypes, tw.default_config.filetypes or {})
+                    -- Remove excluded filetypes
                     opts.filetypes = vim.tbl_filter(function(ft)
                         return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
                     end, opts.filetypes)
+
+                    -- Add included filetypes
                     vim.list_extend(opts.filetypes, opts.filetypes_include or {})
                 end,
             },
         },
     },
 }
-
