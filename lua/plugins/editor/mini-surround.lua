171c07d01e5d4cd920cd73267d65225968375e22
Han Damin <miniex@daminstudio.net>
2026-05-11 10:46:21 +0900

    feat(editor): add mini.surround under gs prefix
    Default mappings use s*, but flash already owns s in
    normal/visual/operator-pending modes, so the prefix is remapped
    to gs* (gsa/gsd/gsr/gsf/gsF/gsh/gsn).

---
+++ lua/plugins/editor/mini-surround.lua
@@ -0,0 +1,36 @@
+-- mini.surround: add / delete / replace surrounding pairs (quotes, brackets,
+-- tags). Uses a `gs*` prefix because flash.nvim already owns `s` in normal,
+-- visual, and operator-pending modes.
+return {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+        local opts =
+            require("lazy.core.plugin").values(require("lazy.core.config").spec.plugins["mini.surround"], "opts", false)
+        local mappings = {
+            { opts.mappings.add, desc = "Add surrounding", mode = { "n", "v" } },
+            { opts.mappings.delete, desc = "Delete surrounding" },
+            { opts.mappings.find, desc = "Find right surrounding" },
+            { opts.mappings.find_left, desc = "Find left surrounding" },
+            { opts.mappings.highlight, desc = "Highlight surrounding" },
+            { opts.mappings.replace, desc = "Replace surrounding" },
+            { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+        }
+        return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+        mappings = {
+            add = "gsa",
+            delete = "gsd",
+            find = "gsf",
+            find_left = "gsF",
+            highlight = "gsh",
+            replace = "gsr",
+            update_n_lines = "gsn",
+            suffix_last = "l",
+            suffix_next = "n",
+        },
+        n_lines = 50,
+        search_method = "cover_or_next",
+        silent = true,
+    },
+}
