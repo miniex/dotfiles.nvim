7c154fc792e123d749c0a57a0a7398a288cdf429
Han Damin <miniex@daminstudio.net>
2026-07-16 09:42:40 +0900

    chore(python): drop the colorcolumn ruler
    - textwidth stays, so gq/gw still wrap at the formatter width

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,6 +1,3 @@
--- PEP 8 ruler at the formatter textwidth; "+0" resolves at draw time, so autocmd order is moot.
-vim.opt_local.colorcolumn = "+0"
-
 -- :VenvSelect (global) lives in config/autocmds.lua; buffer-local key only here.
 vim.keymap.set("n", "<leader>cv", function()
     require("config.python_venv").select()
